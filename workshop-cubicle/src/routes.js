97312b7362b67a3bd922d389ddbf4f7f4b6021ae
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-01-26 20:09:16 +0200

    .

--- workshop-cubicle/src/routes.js
+++ workshop-cubicle/src/routes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 router.get('/404', homeController.getErrorPage);
 
 router.get('/create', cubeController.getCreateCube);
-router.post('/create', cubeController.postCreateCube)
+router.post('/create', cubeController.postCreateCube);
+router.get('/details/:cubeId', cubeController.getCubeDetails);
 
-router.get('/details/:cubeId', cubeController.getCubeDetails)
+router.use('/accessory', accessoryController);
+
 module.exports = router;
