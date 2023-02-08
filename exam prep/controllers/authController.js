abff93bc07fb78981d0d088fb1ef1c25ec576aad
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-02-08 15:37:15 +0200

    Added error handling for register.

--- exam prep/controllers/authController.js
+++ exam prep/controllers/authController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const userService = require('../services/authService');
 
 router.get('/auth/login', (req, res) => {
     res.render('login');
@@ -8,9 +9,21 @@ router.get('/auth/register', (req, res) => {
     res.render('register');
 });
 
-router.post('/auth/register', (req, res) => {
-    console.log(req.body);
-    res.end();
+router.post('/auth/register', async (req, res) => {
+    const { username, password, rePass } = req.body;
+
+    if (password !== rePass) {
+        return res.render('register', { error: 'Passwords dont match'});
+    }
+
+    try {
+        await userService.create({ username, password });
+        res.redirect('/login');
+    } catch (err) {
+        // add db errors
+        return res.render('register', { error: 'Passwords dont match'});
+    }
+
 });
 
 module.exports = router;
